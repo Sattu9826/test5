fd71753c3f9997aa6cdd9fc310ee1be14890e49a
Sattu9826 <103723019+Sattu9826@users.noreply.github.com>
2026-03-11 10:22:05 +0530

    Update Test5Application.java

--- src/main/java/com/test5/Test5Application.java
+++ src/main/java/com/test5/Test5Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test5Application {
 
 	public static void main(String[] args) {
+		int a=11;
 		SpringApplication.run(Test5Application.class, args);
 	}
 
